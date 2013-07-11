d762dbf6c8caf37254fb086857ecc6112c836aed
Rohan Dhamnaskar <rohan@vwp.co>
2013-07-11 19:55:31 +0000

    Changes to web.js to read and Write from index.html

--- web.js
+++ web.js
@@ -1,9 +1,30 @@
+#!/usr/bin/env node
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var read = function(){
+    fs.readFile('index.html', function(err,data){
+	if(err) throw err;
+	var newData = data.toString()
+	//console.log(newData);
+	return newData;
+    })
+}
+   
+
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ 
+    response.send(test());
+    //response.send('Hello World2!');
+
+ 
+
+
 });
 
 var port = process.env.PORT || 5000;
